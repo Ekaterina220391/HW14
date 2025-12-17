11244419666c170867f96ecd59fccfb714487f13
origin <dubnitskaya.katya@mail.ru>
2025-12-17 13:44:56 +0400

    New

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Product phone1 = new Product(123456, "iphone", 11000, "телефоны");
+        Product phone2 = new Product(123000, "iphone", 90000, "телефоны");
+        Product phone3 = new Product(123456, "iphone", 90000, "телефоны");
+        System.out.println(phone1.toString());
+        System.out.println(phone2.toString());
+        System.out.println(phone3.toString());
+
 
     }
 }
