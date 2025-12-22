8be55dde3c36553f65f041de4a528a2a800c7466
origin <dubnitskaya.katya@mail.ru>
2025-12-22 08:29:05 +0400

    New

--- src/Order.java
+++ src/Order.java
@@ -4,14 +4,16 @@ import java.util.Objects;
 public class Order {
     public Product[] basket;
     public String custumer;
+    int i=0;
 
     public Order(String custumer, Product[] basket) {
         this.custumer = custumer;
         this.basket = basket;
 
     }
+    @Override
         public String toString() {
-            return "Покупатель: " + this.custumer + this.basket;
+            return "Покупатель: " + this.custumer + Arrays.toString(basket) ;
 
         }
     @Override
@@ -21,8 +23,29 @@ public class Order {
             return false;
         }
         Order order = (Order) o;
-        return basket == order.basket && Objects.equals(custumer, order.basket);
+        return custumer == order.custumer && Objects.equals(custumer, order.basket);
+        for (int i = 0; i < basket.length; i++) {
+            if (basket != null && basket.length > 0) {
+            }
+        }
     }
+        @Override
+        public boolean equals(Product o ) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return id == product.id && Objects.equals(category, product.category);
+          }
+
+        }
+
+
+
+
+
+
 
 
 
