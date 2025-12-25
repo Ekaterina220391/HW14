1161f8da968ffcace3ccf4484b992c9f0bb8d82a
origin <dubnitskaya.katya@mail.ru>
2025-12-25 11:47:58 +0400

    New

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        Product product1 = new Product(123456, "iphone", 11000, "телефоны");
+        Product product1 = new Product(123456, "iphone", 110000, "телефоны");
         Product product2 = new Product(123000, "iphone", 90000, "телефоны");
         Product product3 = new Product(123456, "iphone", 90000, "телефоны");
+        Product product4 = new Product(123400, "samsung", 80000, "телефоны");
+        Product product5 = new Product(123154, "samsung", 100000, "телефоны");
         System.out.println(product1.toString());
         System.out.println(product2.toString());
         System.out.println(product3.toString());
+        System.out.println(product4.toString());
+        System.out.println(product5.toString());
         System.out.println(product1.equals(product2));
+        System.out.println(product1.equals(product3));
+        System.out.println(product1.equals(product4));
         Product [] arrayProduct = { product1, product2,product3 };
-        Order basket1 = new Order(" Покупатель1 ",arrayProduct);
-        System.out.println(lenght);
+
+
 
 
 
--- src/Order.java
+++ src/Order.java
@@ -9,9 +9,7 @@ public class Order {
     public Order(String custumer, Product[] basket) {
         this.custumer = custumer;
         this.basket = basket;
-
     }
-
     @Override
     public String toString() {
         return "Покупатель: " + this.custumer + Arrays.toString(basket);
@@ -20,21 +18,22 @@ public class Order {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Order order = (Order) o;
-        {
-            if (!Objects.equals(custumer, order.custumer)) return false;
-            if (basket == null && order.basket == null) return true;
-            if (basket == null || order.basket == null) return false;
-            if (basket.length != order.basket.length) return false;
-        }
-        for (int i = 0; i < basket.length; i++) {
-            if (basket[i] == null && basket[i] != null) return false;
-            if (!basket[i].equals(order.basket[i])) return false;
-        }
+        if(o ==null|| getClass() !=o.getClass()) {
+        return false;
+    }
 
+    Order order = (Order) o;
+
+            if(!Objects.equals(custumer,order.custumer))return false;
+            if(basket ==null&&order.basket ==null)return true;
+            if(basket ==null||order.basket ==null)return false;
+            if(basket.length !=order.basket.length)return false;
+
+        for(int i = 0; i<basket.length;i++) {
+        if (basket[i] == null && order.basket[i] != null) return false;
+        if (!basket[i].equals(order.basket[i])) return false;
+    }
+        return true;
     }
 }
 
