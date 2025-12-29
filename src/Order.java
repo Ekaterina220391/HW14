951f24fd099f0dd447125bdc247cde7ff6e7a8ae
origin <dubnitskaya.katya@mail.ru>
2025-12-29 18:56:06 +0400

    New

--- src/Order.java
+++ src/Order.java
@@ -10,6 +10,7 @@ public class Order {
         this.custumer = custumer;
         this.basket = basket;
     }
+
     @Override
     public String toString() {
         return "Покупатель: " + this.custumer + Arrays.toString(basket);
@@ -17,25 +18,45 @@ public class Order {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if(o ==null|| getClass() !=o.getClass()) {
-        return false;
-    }
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
-    Order order = (Order) o;
 
-            if(!Objects.equals(custumer,order.custumer))return false;
-            if(basket ==null&&order.basket ==null)return true;
-            if(basket ==null||order.basket ==null)return false;
-            if(basket.length !=order.basket.length)return false;
+        Order order = (Order) o;
 
-        for(int i = 0; i<basket.length;i++) {
-        if (basket[i] == null && order.basket[i] != null) return false;
-        if (!basket[i].equals(order.basket[i])) return false;
-    }
-        return true;
+        if (!Objects.equals(custumer, order.custumer)) {
+            return false;
+        }
+        if (basket == null && order.basket == null) {
+            return true;
+        }
+        if (basket == null || order.basket == null) {
+            return false;
+        }
+        if (basket.length != order.basket.length) {
+            return false;
+        }
+
+        for (int i = 0; i < basket.length; i++) {
+            if (basket[i] == null && order.basket[i] != null) {
+                return false;
+            }
+            if (!basket[i].equals(order.basket[i])) {
+                return false;
+            }
+            return true;
+        }
     }
 }
+
+
+
+
+
 
 
 
